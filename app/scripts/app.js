39070d03d6de3f74c73413880a8c8ee8d679b47a
Balasubramanian Viswanath <balaviswanath@users.noreply.github.com>
2016-05-15 17:44:37 +0530

    Update app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('confusionApp', ['ui.router','ngResource','ngDialog'])
+angular.module('conaaafusionApp', ['ui.router','ngResource','ngDialog'])
 .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
         
